dc5538d7a97bbed3078834029d088bc079c3c277
Sanjay Murugan <tmsanjay99@gmail.com>
2020-05-28 13:07:44 +0530

    update.

---
+++ app/src/main/java/com/nak/sales/dbPojo.java
@@ -0,0 +1,38 @@
+package com.nak.sales;
+
+public class dbPojo {
+    String date, itemName;
+    int price,id;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
